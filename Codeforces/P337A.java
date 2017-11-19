2312a5091d9e8bf197bd21e3c8162b57c708a87e
bahriddin <bahriddin.abdiev@gmail.com>
2017-11-20 07:42:37 +1100

    337A solved

--- Codeforces/P337A.java
+++ Codeforces/P337A.java
@@ -1,10 +1,29 @@
 package Codeforces;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * @Solution
+ * 1) sort array
+ * 2) find min difference in the range of n elements
+ */
 public class P337A {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int
+        int n = in.nextInt(),
+            m = in.nextInt();
+        int[] f = new int[m];
+        int minDiff = 1000;
+
+        for (int i = 0; i < m; i++)
+            f[i] = in.nextInt();
+
+        Arrays.sort(f);
+        for (int i = n - 1; i < m; i++)
+            if(f[i] - f[i - n + 1] < minDiff)
+                minDiff = f[i] - f[i - n + 1];
+
+        System.out.println(minDiff);
     }
 }
