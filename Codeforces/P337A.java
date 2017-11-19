34135b571d8df077395c6b1e9f80c450b495458d
bahriddin <bahriddin.abdiev@gmail.com>
2017-11-20 07:31:02 +1100

    Initial commit

---
+++ Codeforces/P337A.java
@@ -0,0 +1,10 @@
+package Codeforces;
+
+import java.util.Scanner;
+
+public class P337A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int
+    }
+}
