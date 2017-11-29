fa3d02660be8fefcc368f707804f4ccb04fcbe7a
bahriddin <bahriddin.abdiev@gmail.com>
2017-11-30 08:03:02 +1100

    455A is fixed.

--- Codeforces/P455A.java
+++ Codeforces/P455A.java
@@ -1,7 +1,5 @@
 package Codeforces;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -10,36 +8,23 @@ import java.util.Scanner;
 public class P455A {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt(),
-                tmp;
-        long[] s;
-        HashMap<Integer, Integer> dict = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            tmp = in.nextInt();
-            if (dict.containsKey(tmp))
-                dict.replace(tmp, dict.get(tmp) + 1);
-            else
-                dict.put(tmp, 1);
-        }
-        s = new long[dict.size() + 1];
-        int i = 1;
-        for (Map.Entry<Integer, Integer> entry: dict.entrySet()) {
-            int k = entry.getKey(),
-                v = entry.getValue();
-            tmp = k * v;
-            if (i == 1) {
-                s[i] = tmp;
-            } else if (!dict.containsKey(k - 1)) {
-                s[i] = s[i - 1] + tmp;
-            } else if (s[i - 2] + tmp > s[i - 1]){
-                s[i] = s[i - 2] + tmp;
-            } else {
-                s[i] = s[i - 1];
-            }
+        int n = in.nextInt();
+        long tmp;
+        long[] s = new long[100001];
 
-            i++;
+        for (int i = 0; i < n; i++)
+            s[in.nextInt()]++;
+
+        for (int i = 2; i <= 100000; i++) {
+            tmp = i * s[i];
+            if (s[i-2] + tmp < s[i-1])
+                s[i] = s[i-1];
+            else
+                s[i] = s[i-2] + tmp;
         }
-        System.out.println(s[dict.size()]);
+
+
+        System.out.println(s[100000]);
     }
 
 }
