f224e6f8916fd2477210faa97f5441bd05e433d8
bahriddin <bahriddin.abdiev@gmail.com>
2017-12-27 20:26:06 +1100

    50A is solved.

---
+++ Codeforces/P50A.java
@@ -0,0 +1,12 @@
+package Codeforces;
+
+import java.util.Scanner;
+
+public class P50A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt() * in.nextInt();
+
+        System.out.println(m / 2);
+    }
+}
